466f3c8880b32f8336e34d14e59cd95fbe11abcc
Andrej Hosna <hosna.andrej@gmail.com>
2021-01-24 19:15:16 +0100

    Added DB support and initial scaffolds for model

--- redo-web/config/routes.rb
+++ redo-web/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :faces
+  resources :rotations
+  resources :redos
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
